1bb0e257884cc05db167fe39e288aef83761b5a4
Vinicius de Miranda melo <161662601+vinimiiranda@users.noreply.github.com>
2026-03-10 18:48:10 -0300

    Classe criada para gerenciar informações de verificação de e-mail

---
+++ src/main/java/br/com/forum_hub/domain/autenticacao/github/DadosEmail.java
@@ -0,0 +1,7 @@
+package br.com.forum_hub.domain.autenticacao.github;
+
+public record DadosEmail(String email,
+                         Boolean primary,
+                         Boolean verified,
+                         String visibility) {
+}
