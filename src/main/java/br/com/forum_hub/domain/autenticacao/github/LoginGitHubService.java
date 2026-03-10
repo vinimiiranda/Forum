d8bb43037b2eadeb8c23892911a3fa94270634f2
Vinicius de Miranda melo <161662601+vinimiiranda@users.noreply.github.com>
2026-03-10 17:55:30 -0300

    classe criada para gerenciar codigo da controller

---
+++ src/main/java/br/com/forum_hub/domain/autenticacao/github/LoginGitHubService.java
@@ -0,0 +1,57 @@
+package br.com.forum_hub.domain.autenticacao.github;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.util.Map;
+
+@Service
+public class LoginGitHubService {
+
+    private final String clientId = "Ov23liIS6oyuJl3lOZww";
+    private final String clientSecret = "795f35822db59f65db8c57afeedd81ffa926f85b";
+    private final String redirectUri = "http://localhost:8080/login/github/autorizado";
+    private final RestClient restClient;
+
+    public LoginGitHubService(RestClient.Builder restClientBuilder) {
+        this.restClient = restClientBuilder.build();
+    }
+
+
+    public String gerarUrl() {
+        return "https://github.com/login/oauth/authorize" +
+                "?client_id=" + clientId +
+                "&redirect_uri=" + redirectUri +
+                "&scope=read:user,user:email";
+    }
+
+
+    public String obterToken(String code) {
+        var resposta = restClient.post()
+                .uri("https://github.com/login/oauth/access_token")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Map.of("code", code, "client_id", clientId,
+                        "client_secret", clientSecret, "redirect_uri", redirectUri))
+                .retrieve().body(Map.class);
+        return resposta.get("access_token").toString();
+    }
+
+    public String obterEmail(String code) {
+        var token = obterToken(code);
+
+        var headers = new HttpHeaders();
+        headers.setBearerAuth(token);
+
+        var resposta = restClient.get()
+                .uri("https://api.github.com/user/emails")
+                .headers(httpHeaders -> httpHeaders.addAll(headers))
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve().body(String.class);
+        return resposta;
+    }
+
+
+}
