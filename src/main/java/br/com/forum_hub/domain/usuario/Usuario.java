ea52a0e92e5c906961d8d67cf1af3ef90a1646cb
Vinicius de Miranda melo <161662601+vinimiiranda@users.noreply.github.com>
2026-03-10 23:35:11 -0300

    construtor modificado

--- src/main/java/br/com/forum_hub/domain/usuario/Usuario.java
+++ src/main/java/br/com/forum_hub/domain/usuario/Usuario.java
@@ -43,20 +43,23 @@ public class Usuario implements UserDetails {
     public Usuario() {
     }
 
-    public Usuario(DadosCadastroUsuario dados, String senhaCriptografada, Perfil perfil) {
+    public Usuario(DadosCadastroUsuario dados, String senhaCriptografada, Perfil perfil, Boolean verificado) {
         this.nomeCompleto = dados.nomeCompleto();
         this.email = dados.email();
         this.senha = senhaCriptografada;
         this.nomeUsuario = dados.nomeUsuario();
         this.biografia = dados.biografia();
         this.miniBiografia = dados.miniBiografia();
-        this.verificado = false;
-        this.token = UUID.randomUUID().toString();
-        this.expiracaoToken = LocalDateTime.now().plusMinutes(30);
-        this.ativo = false;
+        if (verificado) {
+            aprovarUsuario();
+        } else {
+            this.verificado = false;
+            this.token = UUID.randomUUID().toString();
+            this.expiracaoToken = LocalDateTime.now().plusMinutes(30);
+            this.ativo = false;
+        }
         this.perfis.add(perfil);
     }
-
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -120,9 +123,7 @@ public class Usuario implements UserDetails {
         if (expiracaoToken.isBefore(LocalDateTime.now())) {
             throw new RegraDeNegocioException("Link de verificação expirou!");
         }
-        this.verificado = true;
-        this.token = null;
-        this.expiracaoToken = null;
+        aprovarUsuario();
     }
 
     public Usuario alterarDados(DadosEdicaoUsuario dados) {
@@ -153,5 +154,12 @@ public class Usuario implements UserDetails {
 
     public void reativar() {
         this.ativo = true;
+    }
+
+    private void aprovarUsuario() {
+        this.verificado = true;
+        this.ativo = true;
+        this.token = null;
+        this.expiracaoToken = null;
     }
 }
