9c74335888e517ae3b0a5841ba00ab12e4f326cd
Vinicius de Miranda melo <161662601+vinimiiranda@users.noreply.github.com>
2026-03-10 23:36:44 -0300

    feita alteração para melhor entendimento do codigo

--- src/main/java/br/com/forum_hub/domain/autenticacao/github/LoginGitHubService.java
+++ src/main/java/br/com/forum_hub/domain/autenticacao/github/LoginGitHubService.java
@@ -1,11 +1,13 @@
 package br.com.forum_hub.domain.autenticacao.github;
 
+import br.com.forum_hub.domain.usuario.DadosCadastroUsuario;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
 import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class LoginGitHubService {
@@ -28,35 +30,58 @@ public class LoginGitHubService {
     }
 
 
-    private String obterToken(String code) {
+    private String obterToken(String code, String id, String uri) {
         var resposta = restClient.post()
                 .uri("https://github.com/login/oauth/access_token")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
-                .body(Map.of("code", code, "client_id", clientId,
-                        "client_secret", clientSecret, "redirect_uri", redirectUri))
+                .body(Map.of("code", code, "client_id", id,
+                        "client_secret", clientSecret, "redirect_uri", uri))
                 .retrieve().body(Map.class);
         return resposta.get("access_token").toString();
     }
 
     public String obterEmail(String code) {
-        var token = obterToken(code);
-
+        var token = obterToken(code, clientId, redirectUri);
         var headers = new HttpHeaders();
-        headers.setBearerAuth(token);
 
+        headers.setBearerAuth(token);
+        return enviarRequisicaoEmail(headers);
+    }
+
+    private String enviarRequisicaoEmail(HttpHeaders headers) {
         var resposta = restClient.get()
                 .uri("https://api.github.com/user/emails")
                 .headers(httpHeaders -> httpHeaders.addAll(headers))
                 .accept(MediaType.APPLICATION_JSON)
-                .retrieve().body(DadosEmail[].class);
+                .retrieve()
+                .body(DadosEmail[].class);
 
-        for (DadosEmail d : resposta) {
-            if (d.primary() && d.verified())
+        for(DadosEmail d: resposta){
+            if(d.primary() && d.verified())
                 return d.email();
         }
         return null;
     }
+    public DadosCadastroUsuario obterDadosOAuth(String codigo){
+        var accessToken = obterToken(codigo,  clientId, redirectUri);
+        var headers = new HttpHeaders();
 
+        headers.setBearerAuth(accessToken);
+
+        var email = enviarRequisicaoEmail(headers);
+        var resposta = restClient.get()
+                .uri("https://api.github.com/user")
+                .headers(httpHeaders -> httpHeaders.addAll(headers))
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .body(Map.class);
+
+        var nomeCompleto = resposta.get("name").toString();
+        var nomeUsuario = resposta.get("login").toString();
+        var senha = UUID.randomUUID().toString();
+        return new DadosCadastroUsuario(email, senha, nomeCompleto, nomeUsuario, null, null);
+
+    }
 
 }
