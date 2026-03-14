dbdd1d3b2847606e7aee2298903ee6edd63580e3
Vinicius de Miranda melo <161662601+vinimiiranda@users.noreply.github.com>
2026-03-14 19:01:57 -0300

    cria RegistroController para cadastro e verificação de email

---
+++ src/main/java/br/com/forum_hub/controller/RegistroController.java
@@ -0,0 +1,33 @@
+package br.com.forum_hub.controller;
+
+import br.com.forum_hub.domain.usuario.DadosCadastroUsuario;
+import br.com.forum_hub.domain.usuario.DadosListagemUsuario;
+import br.com.forum_hub.domain.usuario.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+public class RegistroController {
+
+    private final UsuarioService usuarioService;
+
+
+    public RegistroController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping("/registrar")
+    public ResponseEntity<DadosListagemUsuario> cadastrar(@RequestBody @Valid DadosCadastroUsuario dados, UriComponentsBuilder uriBuilder) {
+        var usuario = usuarioService.cadastrar(dados);
+        var uri = uriBuilder.path("/{nomeUsuario}").buildAndExpand(usuario.getNomeUsuario()).toUri();
+        return ResponseEntity.created(uri).body(new DadosListagemUsuario(usuario));
+    }
+
+    @GetMapping("/verificar-conta")
+    public ResponseEntity<String> verificarEmail(@RequestParam String codigo) {
+        usuarioService.verificarEmail(codigo);
+        return ResponseEntity.ok("Conta verificada com sucesso!");
+    }
+}
