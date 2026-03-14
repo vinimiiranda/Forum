02be04092eae30707047b0085276aa9d524313ef
Vinicius de Miranda melo <161662601+vinimiiranda@users.noreply.github.com>
2026-03-14 19:22:57 -0300

    adiciona suporte a verificação, refresh token e A2F na entidade Usuario

--- src/main/java/br/com/forum_hub/domain/usuario/Usuario.java
+++ src/main/java/br/com/forum_hub/domain/usuario/Usuario.java
@@ -1,9 +1,10 @@
 package br.com.forum_hub.domain.usuario;
 
 import br.com.forum_hub.domain.perfil.Perfil;
-import br.com.forum_hub.domain.perfil.PerfilNome;
 import br.com.forum_hub.infra.exception.RegraDeNegocioException;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -13,6 +14,8 @@ import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
+@Getter
+@NoArgsConstructor
 @Entity
 @Table(name = "usuarios")
 public class Usuario implements UserDetails {
@@ -32,16 +35,14 @@ public class Usuario implements UserDetails {
     private String token;
     private LocalDateTime expiracaoToken;
     private Boolean ativo;
+    private String secret;
+    private Boolean a2fAtiva;
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "usuarios_perfis",
             joinColumns = @JoinColumn(name = "usuario_id"),
             inverseJoinColumns = @JoinColumn(name = "perfil_id"))
     private List<Perfil> perfis = new ArrayList<>();
-
-    @Deprecated
-    public Usuario() {
-    }
 
     public Usuario(DadosCadastroUsuario dados, String senhaCriptografada, Perfil perfil, Boolean verificado) {
         this.nomeCompleto = dados.nomeCompleto();
@@ -95,30 +96,6 @@ public class Usuario implements UserDetails {
         return email;
     }
 
-    public String getNomeCompleto() {
-        return nomeCompleto;
-    }
-
-    public String getNomeUsuario() {
-        return nomeUsuario;
-    }
-
-    public String getBiografia() {
-        return biografia;
-    }
-
-    public String getMiniBiografia() {
-        return miniBiografia;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
     public void verificar() {
         if (expiracaoToken.isBefore(LocalDateTime.now())) {
             throw new RegraDeNegocioException("Link de verificação expirou!");
@@ -143,7 +120,6 @@ public class Usuario implements UserDetails {
         this.senha = senhaCriptografada;
     }
 
-
     public void adcionarPerfil(Perfil perfil) {
         this.perfis.add(perfil);
     }
@@ -161,5 +137,17 @@ public class Usuario implements UserDetails {
         this.ativo = true;
         this.token = null;
         this.expiracaoToken = null;
+    }
+
+    public void gerarSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public boolean isA2fAtiva() {
+        return this.a2fAtiva;
+    }
+
+    public void ativarA2f() {
+        this.a2fAtiva = true;
     }
 }
