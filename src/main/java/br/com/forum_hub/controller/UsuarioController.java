cbc03d200f58afa5fbab84b9965884d0d4c9750a
Vinicius de Miranda melo <161662601+vinimiiranda@users.noreply.github.com>
2026-03-14 19:25:21 -0300

    adiciona controller de gerenciamento de usuário

--- src/main/java/br/com/forum_hub/controller/UsuarioController.java
+++ src/main/java/br/com/forum_hub/controller/UsuarioController.java
@@ -4,64 +4,40 @@ package br.com.forum_hub.controller;
 import br.com.forum_hub.domain.perfil.DadosPerfil;
 import br.com.forum_hub.domain.usuario.*;
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
 
+@AllArgsConstructor
 @RestController
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
 
-    public UsuarioController(UsuarioService usuarioService) {
-        this.usuarioService = usuarioService;
-    }
-
-
-    @PostMapping("/registrar")
-    public ResponseEntity<DadosListagemUsuario> cadastrar(@RequestBody @Valid DadosCadastroUsuario dados, UriComponentsBuilder uriBuilder) {
-        var usuario = usuarioService.cadastrar(dados);
-        var uri = uriBuilder.path("/{usuarios}").buildAndExpand(usuario.getNomeUsuario()).toUri();
-        return ResponseEntity.created(uri).body(new DadosListagemUsuario(usuario));
-    }
-
-    @GetMapping("/verificar-conta")
-    public ResponseEntity<String> verifcarEmail(@RequestBody String codigo) {
-        usuarioService.verificarEmail(codigo);
-        return ResponseEntity.ok("Conta verificada com sucesso!");
+    @GetMapping("/{nomeUsuario}")
+    public ResponseEntity<DadosListagemUsuario> exibirPerfil(@PathVariable String nomeUsuario){
+        var usuario = usuarioService.buscarPeloNomeUsuario(nomeUsuario);
+        return ResponseEntity.ok(new DadosListagemUsuario(usuario));
     }
 
     @PutMapping("/editar-perfil")
-    public ResponseEntity<DadosListagemUsuario> editarPerfil(@RequestBody @Valid DadosEdicaoUsuario dados, @AuthenticationPrincipal Usuario logado) {
+    public ResponseEntity<DadosListagemUsuario> editarPerfil(@RequestBody @Valid DadosEdicaoUsuario dados, @AuthenticationPrincipal Usuario logado){
         var usuario = usuarioService.editarPerfil(logado, dados);
         return ResponseEntity.ok(new DadosListagemUsuario(usuario));
     }
 
     @PatchMapping("alterar-senha")
-    public ResponseEntity<Void> alterarSenha(@RequestBody @Valid DadosAlteracaoSenha dados, @AuthenticationPrincipal Usuario logado) {
+    public ResponseEntity<Void> alterarSenha(@RequestBody @Valid DadosAlteracaoSenha dados, @AuthenticationPrincipal Usuario logado){
         usuarioService.alterarSenha(dados, logado);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/desativar")
-    public ResponseEntity<Void> banirUsuario(@AuthenticationPrincipal Usuario logado) {
-        usuarioService.desativarUsuario(logado);
+    public ResponseEntity<Void> desativarUsuario(@PathVariable Long id, @AuthenticationPrincipal Usuario logado){
+        usuarioService.desativarUsuario(id,logado);
         return ResponseEntity.noContent().build();
     }
-
-    @PatchMapping("/adicionar-perfil/{id}")
-    public ResponseEntity<DadosListagemUsuario> adicionarPerfil(@PathVariable Long id, @RequestBody @Valid DadosPerfil dados) {
-        var usuario = usuarioService.adicionarPerfil(id, dados);
-        return ResponseEntity.ok(new DadosListagemUsuario(usuario));
-    }
-
-    @PatchMapping("/remover-perfil/{id}")
-    public ResponseEntity<DadosListagemUsuario> removerPerfil(@PathVariable Long id, @RequestBody @Valid DadosPerfil dados) {
-        var usuario = usuarioService.removerPerfil(id, dados);
-        return ResponseEntity.ok(new DadosListagemUsuario(usuario));
-    }
-
 
     @PatchMapping("/reativar-conta/{id}")
     public ResponseEntity<Void> reativarUsuario(@PathVariable Long id){
@@ -69,7 +45,9 @@ public class UsuarioController {
         return ResponseEntity.noContent().build();
     }
 
-
-
-
+    @PatchMapping("adicionar-perfil/{id}")
+    public ResponseEntity<DadosListagemUsuario> adicionarPerfil(@PathVariable Long id, @RequestBody @Valid DadosPerfil dados){
+        var usuario = usuarioService.adicionarPerfil(id, dados);
+        return ResponseEntity.ok(new DadosListagemUsuario(usuario));
+    }
 }
