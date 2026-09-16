ba2713dbce6db345db325277ef0ffc5ab77a9e40
Vinicius de Miranda melo <161662601+vinimiiranda@users.noreply.github.com>
2026-09-16 16:06:08 -0300

    refactor: move o segredo do JWT para configuracao externa

--- src/main/java/br/com/forum_hub/domain/autenticacao/TokenService.java
+++ src/main/java/br/com/forum_hub/domain/autenticacao/TokenService.java
@@ -8,23 +8,28 @@ import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
-
 @Service
 public class TokenService {
 
+    private static final String ISSUER = "Forum Hub";
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
     public String gerarToken(Usuario usuario) {
         try {
-            Algorithm algorithm = Algorithm.HMAC256("12345678");
             return JWT.create()
-                    .withIssuer("Forum Hub")
+                    .withIssuer(ISSUER)
                     .withSubject(usuario.getUsername())
                     .withExpiresAt(expiracao(30))
-                    .sign(algorithm);
+                    .sign(algoritmo());
         } catch (JWTCreationException exception) {
             throw new RegraDeNegocioException("Erro ao gerar token JWT de acesso! ");
         }
@@ -32,12 +37,11 @@ public class TokenService {
 
     public String gerarRefreshToken(Usuario usuario) {
         try {
-            Algorithm algorithm = Algorithm.HMAC256("12345678");
             return JWT.create()
-                    .withIssuer("Forum Hub")
-                    .withSubject(usuario.getUsername().toString())
+                    .withIssuer(ISSUER)
+                    .withSubject(usuario.getUsername())
                     .withExpiresAt(expiracao(120))
-                    .sign(algorithm);
+                    .sign(algoritmo());
         } catch (JWTCreationException exception) {
             throw new RegraDeNegocioException("Erro ao gerar token JWT de acesso! ");
         }
@@ -46,15 +50,18 @@ public class TokenService {
     public String verificarToken(String token) {
         DecodedJWT decodedJWT;
         try {
-            Algorithm algorithm = Algorithm.HMAC256("12345678");
-            JWTVerifier verifier = JWT.require(algorithm)
-                    .withIssuer("Forum Hub")
+            JWTVerifier verifier = JWT.require(algoritmo())
+                    .withIssuer(ISSUER)
                     .build();
             decodedJWT = verifier.verify(token);
             return decodedJWT.getSubject();
         } catch (JWTVerificationException exception) {
             throw new RegraDeNegocioException("Erro ao verificar token JWT de acesso! ");
         }
+    }
+
+    private Algorithm algoritmo() {
+        return Algorithm.HMAC256(secret);
     }
 
     private Instant expiracao(Integer minutos) {
